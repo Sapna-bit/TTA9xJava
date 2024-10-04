cb0ff8b4eeee795c997f4dc856b87b5ca6306196
sapna <sapnap97@gmail.com>
2024-10-04 22:41:46 +0530

    commit

---
+++ src/oct_24/ex_0210/Lab_001.java
@@ -0,0 +1,8 @@
+package oct_24.ex_0210;
+
+public class Lab_001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
---
+++ src/oct_24/ex_0310/Lab_003.java
@@ -0,0 +1,12 @@
+package oct_24.ex_0310;
+
+public class Lab_003 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println("Sum: " + (a+b));
+        System.out.println("Difference: " + (a-b));
+        System.out.println("Product: " + (a*b));
+        System.out.println("Quotient: " + (a/b));
+    }
+}
